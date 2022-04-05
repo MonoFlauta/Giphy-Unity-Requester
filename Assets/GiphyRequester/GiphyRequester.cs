857ea14f0f009233d936d218dcd28ed939af2850
Facundo Balboa <monoflauta@gmail.com>
2022-04-05 19:34:14 -0300

    Added comments

--- Assets/GiphyRequester/GiphyRequester.cs
+++ Assets/GiphyRequester/GiphyRequester.cs
@@ -19,18 +19,44 @@ namespace GiphyRequester
             }
         }
         
-        public static IObservable<GiphyMultipleSearchRequest> Search(string query, int limit = 5, int offset = 5, string language = "en", Rating rating = Rating.General) =>
+        /// <summary>
+        /// Searches for a set of gifs
+        /// </summary>
+        /// <param name="query">Query</param>
+        /// <param name="limit">Max amount of gifs. Default is 5</param>
+        /// <param name="offset">Offset for the gifs. Default is 0</param>
+        /// <param name="language">Language. Default is en</param>
+        /// <param name="rating">Rating. Default is General</param>
+        /// <returns>Returns the observable with GiphyMultipleSearchRequest</returns>
+        public static IObservable<GiphyMultipleSearchRequest> Search(string query, int limit = 5, int offset = 0, string language = "en", Rating rating = Rating.General) =>
             Get($"https://api.giphy.com/v1/gifs/search?api_key={Settings.apiKey}&q={query}&limit={limit}&offset={offset}&rating={RatingToString(rating)}&lang={language}")
                 .Select(JsonUtility.FromJson<GiphyMultipleSearchRequest>);
 
+        /// <summary>
+        /// Searches for a unique gif using the Translate API
+        /// </summary>
+        /// <param name="query">Query</param>
+        /// <returns>Returns the observable with GiphySingleSearchRequest</returns>
         public static IObservable<GiphySingleSearchRequest> Translate(string query) =>
             Get($"https://api.giphy.com/v1/gifs/translate?api_key={Settings.apiKey}&s={query}")
                 .Select(JsonUtility.FromJson<GiphySingleSearchRequest>);
 
+        /// <summary>
+        /// Searches for Trending gifs
+        /// </summary>
+        /// <param name="limit">Max amount of gifs. Default is 10</param>
+        /// <param name="rating">Rating. Default is General</param>
+        /// <returns>Returns the observable with GiphyMultipleSearchRequest</returns>
         public static IObservable<GiphyMultipleSearchRequest> Trending(int limit = 10, Rating rating = Rating.General) =>
             Get($"https://api.giphy.com/v1/gifs/trending?api_key={Settings.apiKey}&limit={limit}&rating={RatingToString(rating)}")
                 .Select(JsonUtility.FromJson<GiphyMultipleSearchRequest>);
 
+        /// <summary>
+        /// Searches for random gifs
+        /// </summary>
+        /// <param name="tag">Tag. Default is an empty string</param>
+        /// <param name="rating">Rating. Default is General</param>
+        /// <returns>Returns the observable with GiphySingleSearchRequest</returns>
         public static IObservable<GiphySingleSearchRequest> Random(string tag = "", Rating rating = Rating.General) =>
             Get($"https://api.giphy.com/v1/gifs/random?api_key={Settings.apiKey}&tag={tag}&rating={RatingToString(rating)}")
                 .Select(JsonUtility.FromJson<GiphySingleSearchRequest>);
